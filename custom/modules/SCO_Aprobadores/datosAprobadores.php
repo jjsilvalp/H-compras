c666ea8a516f1ebb8a86b74ba194b48dc53b4eff
jjsilvalp <jsilva@hansa.com.bo>
2017-08-05 00:51:12 -0400

    jsilva
    cambios

--- custom/modules/SCO_Aprobadores/datosAprobadores.php
+++ custom/modules/SCO_Aprobadores/datosAprobadores.php
@@ -4,13 +4,12 @@ class CldatosA
 {
   function Fndatosa($bean, $event, $arguments)
   {
-    //puebla datos proveedor
+    //pobla datos proveedor
     $user_id = $bean->user_id_c;
     $bean_user = BeanFactory::getBean('Users', $user_id);
     $bean->name = $bean_user->user_name;
     $bean->apr_titulo = $bean_user->title;
-    $bean->save(); 
-    //cambio en el archivo
+    $bean->save();
   } 
 }
 ?>
